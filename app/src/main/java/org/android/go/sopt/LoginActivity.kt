4e5e04dd123218b466025bf0e937719a19114b9b
Sangho Kim <97405341+Marchbreeze@users.noreply.github.com>
2023-04-04 11:24:03 +0900

    feat : basic assignment
    필수과제 초안 제출입니다

---
+++ app/src/main/java/org/android/go/sopt/LoginActivity.kt
@@ -0,0 +1,86 @@
+package org.android.go.sopt
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.material.snackbar.Snackbar
+import org.android.go.sopt.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var id: String
+    private lateinit var pw: String
+    private lateinit var name: String
+    private lateinit var skill: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 회원가입 액티비티에서 반환된 intent에서 결과값 받아옴
+        resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    Log.d("sangho", "successful signup")
+                    Snackbar.make(
+                        binding.root,
+                        "회원가입에 성공하였습니다.",
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+
+                    // 전달 받은 result 데이터의 String 가져옴
+                    id = result.data?.getStringExtra("id") ?: ""
+                    pw = result.data?.getStringExtra("pw") ?: ""
+                    name = result.data?.getStringExtra("name") ?: ""
+                    skill = result.data?.getStringExtra("skill") ?: ""
+
+                    Log.d("sangho", "$id, $pw, $name, $skill")
+                }
+            }
+
+        // 회원가입 버튼 클릭 시 이동
+        binding.btnSignUp.setOnClickListener {
+            Log.d("sangho", "signup button clicked")
+
+            val intent = Intent(this, SignUpActivity::class.java)
+            resultLauncher.launch(intent)
+        }
+
+        // 로그인 버튼 클릭 시 이동
+        binding.btnLogin.setOnClickListener {
+            Log.d("sangho", "login button clicked")
+
+            val idEntered = binding.etLoginId.text.toString()
+            val pwEntered = binding.etLoginPw.text.toString()
+
+            // 로그인 조건 확인
+            if (idEntered == id && pwEntered == pw) {
+                Log.d("sangho", "able to login")
+
+                val intent = Intent(this, MyPageActivity::class.java).apply {
+                    putExtra("id", id)
+                    putExtra("pw", pw)
+                    putExtra("name", name)
+                    putExtra("skill", skill)
+                }
+                Toast.makeText(this, "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show()
+                startActivity(intent)
+
+            } else {
+                Log.d("sangho", "unable to login")
+                Snackbar.make(
+                    binding.root,
+                    "회원가입 정보와 일치하지 않습니다.",
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/org/android/go/sopt/SignUpActivity.kt
@@ -0,0 +1,54 @@
+package org.android.go.sopt
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.android.material.snackbar.Snackbar
+import org.android.go.sopt.databinding.ActivitySignUpBinding
+
+class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // SignUp 버튼 클릭 시
+        binding.btnSignUp.setOnClickListener {
+            Log.d("sangho", "signup button clicked")
+
+            val id = binding.etSignUpId.text.toString()
+            val pw = binding.etSignUpPw.text.toString()
+            val name = binding.etSignUpName.text.toString()
+            val skill = binding.etSignUpSkill.text.toString()
+
+            // 로그인 조건 설정
+            if (id.length in 6..10 && pw.length in 8..12) {
+                Log.d("sangho", "able to signup")
+
+                // 반환할 인텐트 설정
+                val intent = Intent(this, SignUpActivity::class.java).apply {
+                    putExtra("id", id)
+                    putExtra("pw", pw)
+                    putExtra("name", name)
+                    putExtra("skill", skill)
+                }
+
+                Log.d("sangho", "$id, $pw, $name, $skill")
+                setResult(RESULT_OK, intent)
+                finish()
+
+            } else {
+                Log.d("sangho", "unable to signup")
+
+                Snackbar.make(
+                    binding.root,
+                    "조건에 맞추어 작성해주세요.",
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+}
