dda6bd659ea03446fc012f8d499fc687dfbbb0b3
Sangho Kim <marchbreeze03@gmail.com>
2023-04-07 00:22:25 +0900

    refactor : code review
    코드리뷰 지적받은 부분들 수정 완료 (EncryptedSharedPreference 빼고)

--- app/src/main/java/org/android/go/sopt/LoginActivity.kt
+++ app/src/main/java/org/android/go/sopt/LoginActivity.kt
@@ -4,14 +4,9 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.preference.PreferenceManager
-import android.util.Log
-import android.view.inputmethod.InputMethod
 import android.view.inputmethod.InputMethodManager
-import android.view.inputmethod.InputMethodManager.HIDE_IMPLICIT_ONLY
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -38,71 +33,17 @@ class LoginActivity : AppCompatActivity() {
         editor = sharedPreferences.edit()
 
         // 회원가입 액티비티에서 반환된 intent에서 결과값 받아옴
-        resultLauncher =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-                if (result.resultCode == Activity.RESULT_OK) {
-                    Log.d("sangho", "successful signup")
-                    Snackbar.make(
-                        binding.root,
-                        "회원가입에 성공하였습니다.",
-                        Snackbar.LENGTH_SHORT
-                    ).show()
-
-                    // 전달 받은 result 데이터의 String 가져옴
-                    id = result.data?.getStringExtra("id") ?: ""
-                    pw = result.data?.getStringExtra("pw") ?: ""
-                    name = result.data?.getStringExtra("name") ?: ""
-                    skill = result.data?.getStringExtra("skill") ?: ""
-
-                    Log.d("sangho", "$id, $pw, $name, $skill")
-                }
-            }
+        returnIntent()
 
         // 회원가입 버튼 클릭 시 이동
         binding.btnSignUp.setOnClickListener {
-            Log.d("sangho", "signup button clicked")
-
             val intent = Intent(this, SignUpActivity::class.java)
             resultLauncher.launch(intent)
         }
 
         // 로그인 버튼 클릭 시 이동
         binding.btnLogin.setOnClickListener {
-            Log.d("sangho", "login button clicked")
-
-            val idEntered = binding.etLoginId.text.toString()
-            val pwEntered = binding.etLoginPw.text.toString()
-
-            // 로그인 조건 확인
-            if (idEntered == id && pwEntered == pw) {
-                Log.d("sangho", "able to login")
-
-                val intent = Intent(this, MyPageActivity::class.java).apply {
-                    putExtra("id", id)
-                    putExtra("pw", pw)
-                    putExtra("name", name)
-                    putExtra("skill", skill)
-                }
-                Toast.makeText(this, "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show()
-
-                // 로그인 성공한 정보는 자동로그인을 위해 저장
-                editor.putString("id", id)
-                editor.putString("pw", pw)
-                editor.apply()
-
-                // 로그인 액티비티 종료 후 자기소개 화면으로 넘어가기
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
-                startActivity(intent)
-                finish()
-
-            } else {
-                Log.d("sangho", "unable to login")
-                Snackbar.make(
-                    binding.root,
-                    "회원가입 정보와 일치하지 않습니다.",
-                    Snackbar.LENGTH_SHORT
-                ).show()
-            }
+            login()
         }
 
         // 화면 터치로 키보드 내리기
@@ -111,23 +52,80 @@ class LoginActivity : AppCompatActivity() {
         }
 
         // 자동로그인 설정
+        autologin()
+    }
+
+    private fun hideKeyboard(activity: Activity) {
+        val keyboard = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        keyboard.hideSoftInputFromWindow(activity.window.decorView.applicationWindowToken, 0)
+    }
+
+    private fun login() {
+        val idEntered = binding.etLoginId.text.toString()
+        val pwEntered = binding.etLoginPw.text.toString()
+
+        // 로그인 조건 확인
+        if (idEntered == id && pwEntered == pw) {
+
+            val intent = Intent(this, MyPageActivity::class.java).apply {
+                putExtra("id", id)
+                putExtra("pw", pw)
+                putExtra("name", name)
+                putExtra("skill", skill)
+            }
+            Toast.makeText(this, getString(R.string.toast_login), Toast.LENGTH_SHORT).show()
+
+            // 로그인 성공한 정보는 자동로그인을 위해 저장
+            editor.putString("id", id)
+            editor.putString("pw", pw)
+            editor.apply()
+
+            // 로그인 액티비티 종료 후 자기소개 화면으로 넘어가기
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finish()
+
+        } else {
+            Snackbar.make(
+                binding.root,
+                getString(R.string.snackbar_cant_login),
+                Snackbar.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    // 회원가입 액티비티에서 반환된 intent에서 결과값 받아옴
+    private fun returnIntent() {
+        resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    Snackbar.make(
+                        binding.root,
+                        getString(R.string.snackbar_sign_up),
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+
+                    // 전달 받은 result 데이터의 String 가져옴
+                    id = result.data?.getStringExtra("id") ?: ""
+                    pw = result.data?.getStringExtra("pw") ?: ""
+                    name = result.data?.getStringExtra("name") ?: ""
+                    skill = result.data?.getStringExtra("skill") ?: ""
+                }
+            }
+    }
+
+    private fun autologin() {
         val idShared = sharedPreferences.getString("id", null)
         val pwShared = sharedPreferences.getString("pw", null)
         if (idShared != null && pwShared != null) {
-            Log.d("sangho", "sharedPreference -> id=$idShared, pw=$pwShared")
             val intent = Intent(this, MyPageActivity::class.java).apply {
                 putExtra("id", idShared)
                 putExtra("pw", pwShared)
             }
-            Toast.makeText(this, "자동 로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, getString(R.string.toast_auto_login), Toast.LENGTH_SHORT).show()
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(intent)
             finish()
         }
-    }
-
-    private fun hideKeyboard(activity: Activity){
-        val keyboard = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        keyboard.hideSoftInputFromWindow(activity.window.decorView.applicationWindowToken, 0)
     }
 }
--- app/src/main/java/org/android/go/sopt/SignUpActivity.kt
+++ app/src/main/java/org/android/go/sopt/SignUpActivity.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.inputmethod.InputMethodManager
 import com.google.android.material.snackbar.Snackbar
 import org.android.go.sopt.databinding.ActivitySignUpBinding
@@ -19,41 +18,11 @@ class SignUpActivity : AppCompatActivity() {
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // SignUp 버튼 클릭 시
+        // SignUp 버튼 클릭
         binding.btnSignUp.setOnClickListener {
-            Log.d("sangho", "signup button clicked")
-
-            val id = binding.etSignUpId.text.toString()
-            val pw = binding.etSignUpPw.text.toString()
-            val name = binding.etSignUpName.text.toString()
-            val skill = binding.etSignUpSkill.text.toString()
-
-            // 로그인 조건 설정 후 가입 마무리
-            if (id.length in 6..10 && pw.length in 8..12) {
-                Log.d("sangho", "able to signup")
-
-                // 반환할 인텐트 설정
-                val intent = Intent(this, SignUpActivity::class.java).apply {
-                    putExtra("id", id)
-                    putExtra("pw", pw)
-                    putExtra("name", name)
-                    putExtra("skill", skill)
-                }
-
-                Log.d("sangho", "$id, $pw, $name, $skill")
-                setResult(RESULT_OK, intent)
-                finish()
-
-            } else {
-                Log.d("sangho", "unable to signup")
-
-                Snackbar.make(
-                    binding.root,
-                    "조건에 맞추어 작성해주세요.",
-                    Snackbar.LENGTH_SHORT
-                ).show()
-            }
+            signInByRule()
         }
+        
         // 화면 터치로 키보드 내리기
         binding.root.setOnClickListener {
             hideKeyboard(this)
@@ -68,8 +37,35 @@ class SignUpActivity : AppCompatActivity() {
             })
     }
 
-    private fun hideKeyboard(activity: Activity){
+    private fun hideKeyboard(activity: Activity) {
         val keyboard = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         keyboard.hideSoftInputFromWindow(activity.window.decorView.applicationWindowToken, 0)
+    }
+
+    private fun signInByRule() {
+        // 로그인 조건 설정 후 가입 마무리
+        val id = binding.etSignUpId.text.toString()
+        val pw = binding.etSignUpPw.text.toString()
+        val name = binding.etSignUpName.text.toString()
+        val skill = binding.etSignUpSkill.text.toString()
+
+        if (id.length in 6..10 && pw.length in 8..12) {
+            // 반환할 인텐트 설정
+            val intent = Intent(this, SignUpActivity::class.java).apply {
+                putExtra("id", id)
+                putExtra("pw", pw)
+                putExtra("name", name)
+                putExtra("skill", skill)
+            }
+            setResult(RESULT_OK, intent)
+            finish()
+
+        } else {
+            Snackbar.make(
+                binding.root,
+                getString(R.string.snackbar_sign_up_rule),
+                Snackbar.LENGTH_SHORT
+            ).show()
+        }
     }
 }
