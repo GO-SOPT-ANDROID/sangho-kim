fdef89d1e893e2036048e48948264cf512e5f743
Sangho Kim <marchbreeze03@gmail.com>
2023-04-12 17:03:22 +0900

    test : update mockplaylist
    스크롤이 가능하도록 예시데이터 많이 설정 (내 취향 담아서)

--- app/src/main/java/org/android/go/sopt/playlist/mockPlayList.kt
+++ app/src/main/java/org/android/go/sopt/playlist/mockPlayList.kt
@@ -4,13 +4,43 @@ import org.android.go.sopt.R
 
 val mockPlayList = listOf<PlaylistSong>(
     PlaylistSong(
-        album = R.drawable.ic_launcher_background,
+        album = R.drawable.ic_song_lacuna,
         artist = "Lacuna",
         title = "Far Away",
     ),
     PlaylistSong(
-        album = R.drawable.ic_launcher_background,
+        album = R.drawable.ic_song_yuri,
         artist = "최유리",
-        title = "동네",
+        title = "욕심의 반대편으로",
+    ),
+    PlaylistSong(
+        album = R.drawable.ic_song_yebit,
+        artist = "예빛",
+        title = "On-Air",
+    ),
+    PlaylistSong(
+        album = R.drawable.ic_song_sininryu,
+        artist = "신인류",
+        title = "날씨의 요정",
+    ),
+    PlaylistSong(
+        album = R.drawable.ic_song_soeun,
+        artist = "박소은",
+        title = "우리는 같은 음악을 듣고",
+    ),
+    PlaylistSong(
+        album = R.drawable.ic_song_hyunsang,
+        artist = "하현상",
+        title = "등대",
+    ),
+    PlaylistSong(
+        album = R.drawable.ic_song_basecamp,
+        artist = "basecamp",
+        title = "How to say (feat. Dvwn)",
+    ),
+    PlaylistSong(
+        album = R.drawable.ic_song_dosii,
+        artist = "dosii",
+        title = "너의궤도"
     )
 )
