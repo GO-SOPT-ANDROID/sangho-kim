6ff6ea5c123016a8f7dd71aafd3b381f31257bac
Sangho Kim <marchbreeze03@gmail.com>
2023-05-15 18:30:23 +0900

    [mod] set function to private
    private 설정이 안되어있는 메서드 수정

--- app/src/main/java/org/android/go/sopt/presentation/auth/LoginActivity.kt
+++ app/src/main/java/org/android/go/sopt/presentation/auth/LoginActivity.kt
@@ -99,7 +99,7 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    fun startActivityWithFlags(intent: Intent) {
+    private fun startActivityWithFlags(intent: Intent) {
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
         startActivity(intent)
         finish()
