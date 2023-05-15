7dcb9ca1a771f3eaea5747f6a428934de138dc35
Sangho Kim <marchbreeze03@gmail.com>
2023-05-15 17:34:15 +0900

    [mod] image load by coil
    Glide 대신 Coil 라이브러리를 활용해 이미지 로드 구현

--- app/src/main/java/org/android/go/sopt/presentation/follower/FollowerViewHolder.kt
+++ app/src/main/java/org/android/go/sopt/presentation/follower/FollowerViewHolder.kt
@@ -1,7 +1,8 @@
 package org.android.go.sopt.presentation.follower
 
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
+import coil.api.load
+import coil.transform.RoundedCornersTransformation
 import org.android.go.sopt.databinding.ItemFollowerBinding
 import org.android.go.sopt.remote.follower.FollowerResponseDTO
 
@@ -9,9 +10,11 @@ class FollowerViewHolder(val binding: ItemFollowerBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
     fun onBind(item: FollowerResponseDTO.User) {
-        Glide.with(binding.root.context).load(item.avatar).into(binding.ivFollowerImage)
         val name = item.firstName + " " + item.lastName
         binding.tvFollowerName.text = name
         binding.tvFollowerEmail.text = item.email
+        binding.ivFollowerImage.load(item.avatar) {
+            transformations(RoundedCornersTransformation(10.0F))
+        }
     }
 }
