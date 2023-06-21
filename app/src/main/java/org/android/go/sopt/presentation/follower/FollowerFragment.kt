041ae5adb417626c5270882958cd4ccb7e0fa6a4
Sangho Kim <marchbreeze03@gmail.com>
2023-06-21 19:01:54 +0900

    #13 / [MOD] Logout 버튼 Follower Fragment로 이동

--- app/src/main/java/org/android/go/sopt/presentation/follower/FollowerFragment.kt
+++ app/src/main/java/org/android/go/sopt/presentation/follower/FollowerFragment.kt
@@ -1,11 +1,16 @@
 package org.android.go.sopt.presentation.follower
 
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.viewModels
 import org.android.go.sopt.R
 import org.android.go.sopt.databinding.FragmentFollowerBinding
 import org.android.go.sopt.data.remote.FollowerResponseDTO
+import org.android.go.sopt.presentation.auth.LoginActivity
 import org.android.go.sopt.presentation.dialog.LoadingDialogFragment
 import org.android.go.sopt.util.base.BindingFragment
 import org.android.go.sopt.util.extension.makeSnackBar
@@ -18,6 +23,7 @@ class FollowerFragment : BindingFragment<FragmentFollowerBinding>(R.layout.fragm
     private val followerAdapter = FollowerAdapter()
 
     private lateinit var loadingDialogFragment: LoadingDialogFragment
+    private lateinit var alertEventHandler : DialogInterface.OnClickListener
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -34,6 +40,12 @@ class FollowerFragment : BindingFragment<FragmentFollowerBinding>(R.layout.fragm
         // 서버 통신으로 User 리스트 받아오기
         viewModel.addListFromServer(1)
         viewModel.addListFromServer(2)
+
+        // 로그아웃 다이얼로그 제어할 이벤트 핸들러 설정
+        binding.btnLogout.setOnClickListener {
+            setAlertEventHandler()
+            buildAlertDialog(alertEventHandler)
+        }
     }
 
     private fun startLoadingDialog() {
@@ -60,6 +72,35 @@ class FollowerFragment : BindingFragment<FragmentFollowerBinding>(R.layout.fragm
         viewModel.errorResult.observe(viewLifecycleOwner) { errorResult ->
             Timber.d("서버 통신 실패 : $errorResult")
             binding.root.makeSnackBar(getString(R.string.snackbar_server_failure))
+        }
+    }
+
+    private fun setAlertEventHandler(): DialogInterface.OnClickListener {
+        alertEventHandler = DialogInterface.OnClickListener { _, click ->
+            if (click == DialogInterface.BUTTON_POSITIVE) {
+
+                val sharedPreferences = context?.getSharedPreferences("loginInfo",
+                    Context.MODE_PRIVATE
+                )
+                val editor = sharedPreferences?.edit()
+                editor?.clear()
+                editor?.apply()
+
+                val intent = Intent(context, LoginActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
+        }
+        return alertEventHandler
+    }
+
+    private fun buildAlertDialog(alertEventHandler : DialogInterface.OnClickListener) {
+        AlertDialog.Builder(this.requireContext()).run {
+            setTitle(resources.getString(R.string.dialog_account_logout_title))
+            setMessage(resources.getString(R.string.dialog_account_logout_text))
+            setPositiveButton("YES", alertEventHandler)
+            setNegativeButton("NO", alertEventHandler)
+            show()
         }
     }
 }
