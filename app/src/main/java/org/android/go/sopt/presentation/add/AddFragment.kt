c06e1d70e1fb3a302dee18ea89acdd6ebc50f067
Sangho Kim <marchbreeze03@gmail.com>
2023-06-22 21:01:03 +0900

    #13 / [ADD] PhotoPicker 구현

--- app/src/main/java/org/android/go/sopt/presentation/add/AddFragment.kt
+++ app/src/main/java/org/android/go/sopt/presentation/add/AddFragment.kt
@@ -1,15 +1,29 @@
 package org.android.go.sopt.presentation.add
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import coil.load
 import org.android.go.sopt.R
 import org.android.go.sopt.databinding.FragmentAddBinding
 import org.android.go.sopt.util.base.BindingFragment
 
 class AddFragment : BindingFragment<FragmentAddBinding>(R.layout.fragment_add) {
 
+    private val launcher = registerForActivityResult(ActivityResultContracts.PickVisualMedia())
+    { imageUri: Uri? ->
+        binding.ivAddImage.load(imageUri)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.ivAddImage.setOnClickListener{
+            launcher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageAndVideo))
+        }
+
 
     }
 
