2789c34fd65e99b5939b8ba723edbaee219cef6f
Sangho Kim <marchbreeze03@gmail.com>
2023-06-23 16:53:18 +0900

    #13 / [ADD] 곡 추가 이후 재생목록 화면으로 이동

--- app/src/main/java/org/android/go/sopt/presentation/add/AddFragment.kt
+++ app/src/main/java/org/android/go/sopt/presentation/add/AddFragment.kt
@@ -1,16 +1,15 @@
 package org.android.go.sopt.presentation.add
 
-import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.ListFragment
 import androidx.fragment.app.viewModels
 import coil.load
 import org.android.go.sopt.R
 import org.android.go.sopt.databinding.FragmentAddBinding
-import org.android.go.sopt.presentation.auth.LoginActivity
 import org.android.go.sopt.util.ContentUriRequestBody
 import org.android.go.sopt.util.base.BindingFragment
 import org.android.go.sopt.util.extension.makeSnackBar
@@ -32,20 +31,22 @@ class AddFragment : BindingFragment<FragmentAddBinding>(R.layout.fragment_add) {
         binding.vm = viewModel
         binding.lifecycleOwner = this
 
-        binding.btnAddImage.setOnClickListener{
+        binding.btnAddImage.setOnClickListener {
             launcher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
         }
 
         binding.btnAdd.setOnClickListener {
             viewModel.uploadMusic("aaaaa1")
+            observeAddResult()
         }
     }
 
     private fun observeAddResult() {
-        viewModel.addResult.observe(this) { addResult ->
+        viewModel.addResult.observe(viewLifecycleOwner) { addResult ->
             binding.root.makeSnackBar(getString(R.string.snackbar_signup_success))
+            parentFragmentManager.beginTransaction().replace(R.id.fcv_main, ListFragment()).commit()
         }
-        viewModel.errorResult.observe(this) { errorResult ->
+        viewModel.errorResult.observe(viewLifecycleOwner) { errorResult ->
             Timber.d("서버 통신 실패 : $errorResult")
             binding.root.makeSnackBar(getString(R.string.snackbar_server_failure))
         }
