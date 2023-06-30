f041337de029f5e81527a74d574b8ee8851d00ff
Sangho Kim <marchbreeze03@gmail.com>
2023-07-01 00:56:06 +0900

    [ADD/#13] Hilt 적용 위한 사전 설정

--- app/src/main/java/org/android/go/sopt/GoSoptApplication.kt
+++ app/src/main/java/org/android/go/sopt/GoSoptApplication.kt
@@ -1,13 +1,13 @@
-package org.android.go.sopt.util
+package org.android.go.sopt
 
 import android.app.Application
-import org.android.go.sopt.BuildConfig
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
-class TimberApplication : Application() {
+@HiltAndroidApp
+class GoSoptApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
